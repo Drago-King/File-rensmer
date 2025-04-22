de65086476e0e774f95ed95db3f97b07688d7352
Drago-King <106234416+Drago-King@users.noreply.github.com>
2025-04-22 11:18:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
 if __name__ == '__main__':
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.Document.ALL | filters.Audio.ALL | filters.VIDEO, handle_file))
+    app.add_handler(MessageHandler(filters.Document.ALL | filters.attachment.ALL, handle_file))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
     app.add_handler(CallbackQueryHandler(button))
     print("Bot is running...")
